eb45f6bab33fe3d49dd5f8f49ec093f19474df93
iamip <iamipro@naver.com>
2022-07-11 18:55:42 +0900

    스프링 쇼핑 검색 기본

--- src/main/java/com/lms/springcore/SpringcoreApplication.java
+++ src/main/java/com/lms/springcore/SpringcoreApplication.java
@@ -10,9 +10,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan // @WebServlet 어노테이션이 동작하게 함
 @SpringBootApplication
 public class SpringcoreApplication {
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringcoreApplication.class, args);
 
-        
     }
 }
