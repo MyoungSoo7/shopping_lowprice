d1d87b9101fefe2e0b99a6b31125e3cb5f4fc173
iamipro <iamipro@naver.com>
2024-07-12 03:47:35 +0900

    rf

--- src/main/java/com/lms/springcore/SpringcoreApplication.java
+++ src/main/java/com/lms/springcore/SpringcoreApplication.java
@@ -9,10 +9,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@ServletCo
-// mponentScan // @WebServlet 어노테이션이 동작하게 함
-@EnableScheduling   // 스프링부트에서 스케줄러가 작동하게함
-@EnableJpaAuditing  // 시간 자동변경이 가능하게함
+@EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringcoreApplication {
     public static void main(String[] args) {
